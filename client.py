7f3b7825145bd236d47b7a9aa869b3db2da885bd
lucas SERVAIS <servaislucas68@gmail.com>
2023-11-09 16:44:46 +0100

    threads multi clients et fonction arret bye

--- client.py
+++ client.py
@@ -1,23 +1,37 @@
 import socket
 import threading
+import sys
+
+# Flag de fermeture pour indiquer aux threads de s'arrêter
+close_flag = threading.Event()
 
 def send_messages(server_socket):
-    while True:
-        message = input("Enter your message: ")
-        server_socket.send(message.encode())
-        if message.lower() == 'bye':
+    while not close_flag.is_set():
+        try:
+            message = input("Enter your message: ")
+            if message.lower() == 'bye':
+                close_flag.set()
+                server_socket.send(message.encode())
+                break
+            else:
+                server_socket.send(message.encode())
+        except BrokenPipeError:
             break
 
 def receive_messages(server_socket):
-    while True:
+    while not close_flag.is_set():
         try:
             message = server_socket.recv(1024).decode()
-            print(f"Server says: {message}")
-            if message.lower() == "server will be shutdown":
-                print("Server has been shutdown. Exiting.")
+            if message:
+                print(f"\n{message}")
+            else:
+                # Si aucun message n'est reçu, cela signifie que le serveur a fermé la connexion.
+                close_flag.set()
                 break
         except ConnectionResetError:
-            print("Server disconnected abruptly")
+            # Si le serveur se ferme brusquement, un ConnectionResetError sera levé.
+            print("\nServer disconnected abruptly")
+            close_flag.set()
             break
 
 def main():
@@ -31,12 +45,14 @@ def main():
     send_thread = threading.Thread(target=send_messages, args=(server_socket,))
     receive_thread = threading.Thread(target=receive_messages, args=(server_socket,))
 
-    send_thread.start()
     receive_thread.start()
+    send_thread.start()
 
+    # Attendre que les threads se terminent proprement
     send_thread.join()
     receive_thread.join()
 
+    print("Disconnected from the chat server.")
     server_socket.close()
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -1,61 +1,82 @@
 import socket
-import sys
 import threading
+import sys
 
-# Flag pour indiquer à tous les threads de se terminer
+# Ce dictionnaire gardera une trace des connexions clients.
+clients = {}
 shutdown_flag = threading.Event()
 
-def send_messages(client_socket):
-    while not shutdown_flag.is_set():
-        try:
-            server_message = input("Enter your message to client: ")
-            if server_message.lower() == "arret":
-                shutdown_flag.set()
-                print("Server will be shutdown")
-                client_socket.send("Server will be shutdown".encode())
-                client_socket.close()
-                break
-            else:
-                client_socket.send(server_message.encode())
-        except BrokenPipeError:
-            break  # Si le client ferme la connexion
+def broadcast(message, exclude_address=None):
+    for other_client, sock in list(clients.items()):
+        if other_client != exclude_address:
+            try:
+                sock.send(message.encode())
+            except Exception as e:
+                print(f"Error sending message to {other_client}: {e}")
 
-def receive_messages(client_socket):
-    while not shutdown_flag.is_set():
+def handle_client(client_socket, client_address):
+    while True:
         try:
             message = client_socket.recv(1024).decode()
-            if not message:
+            if not message or message.lower() == 'bye':
+                # Informer les autres clients que ce client s'est déconnecté
+                broadcast(f"{client_address} has disconnected from the chat.", exclude_address=client_address)
                 break
-            print(f"Client says: {message}")
-            if message.lower() == "bye":
-                print("Client will be disconnected")
-                client_socket.send("Client will be disconnected".encode())
-                break
+
+            # Afficher le message sur le serveur
+            print(f"{client_address} says: {message}")
+            
+            # Renvoyer le message à tous les autres clients
+            broadcast(f"{client_address} says: {message}", exclude_address=client_address)
+
         except ConnectionResetError:
-            print("Client disconnected abruptly")
+            break
+        except Exception as e:
+            print(f"Error: {e}")
             break
 
-def handle_client(client_socket):
-    send_thread = threading.Thread(target=send_messages, args=(client_socket,))
-    send_thread.start()
-    receive_messages(client_socket)
-    send_thread.join()
+    # Lorsqu'un client se déconnecte
+    print(f"Client {client_address} has disconnected.")
+    client_socket.close()
+    del clients[client_address]
+
+def server_command():
+    while not shutdown_flag.is_set():
+        cmd = input("")
+        if cmd.lower() == 'arret':
+            shutdown_flag.set()
+            broadcast("Server is shutting down now!")
+            print("Server shutdown initiated.")
+            for client_socket in clients.values():
+                client_socket.close()
+            sys.exit()
 
 def main():
     host = 'localhost'
     port = 50000
 
-    server_socket = socket.socket()
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((host, port))
-    server_socket.listen(5)
+    server_socket.listen()
+
     print(f"Server is listening on {host}:{port}")
+
+    # Start the server command thread
+    server_command_thread = threading.Thread(target=server_command)
+    server_command_thread.start()
 
     try:
         while not shutdown_flag.is_set():
             client_socket, client_address = server_socket.accept()
             print(f"Connection from {client_address}")
-            client_thread = threading.Thread(target=handle_client, args=(client_socket,))
+
+            # Ajouter le client à la liste des clients
+            clients[client_address] = client_socket
+
+            # Démarrer un nouveau thread pour gérer la connexion
+            client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
             client_thread.start()
+
     except KeyboardInterrupt:
         print("Server is shutting down due to keyboard interrupt.")
         shutdown_flag.set()

f5665338e82fa9b3c667acd916ad43f59b946a5d
lucas SERVAIS <servaislucas68@gmail.com>
2023-11-20 13:59:44 +0100

    modification synthaxe

--- server.py
+++ server.py
@@ -36,7 +36,7 @@ def initialize_database():
                 username VARCHAR(255) UNIQUE NOT NULL,
                 password VARCHAR(255) NOT NULL,
                 ip_address VARCHAR(255),
-                last_login DATETIME
+                registered DATETIME
             )
         """)
 

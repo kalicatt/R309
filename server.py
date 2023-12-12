f6701516f683d86ac475107d2a3f2f9b9ae7be96
kalicatt <servaislucas68@gmail.com>
2023-12-12 09:58:47 +0100

    enregistrement dans les salons

--- server.py
+++ server.py
@@ -253,89 +253,81 @@ def broadcast(message, sender_username=None):
 # Fonction pour gérer chaque client connecté
 
 def handle_client(client_socket, client_address):
-    username = None  # Ajout pour capturer le nom d'utilisateur
+    username = None
     try:
-        while not shutdown_flag.is_set():
-            cmd_info = client_socket.recv(1024).decode()
-            if not cmd_info:
-                break
+        # Établir la connexion avec la base de données
+        with get_database_connection() as conn:
+            cursor = conn.cursor()
 
-            cmd, username, password = cmd_info.split(' ', 2)
+            # Boucle principale pour gérer les commandes du client
+            while not shutdown_flag.is_set():
+                cmd_info = client_socket.recv(1024).decode()
+                if not cmd_info:
+                    break
 
-            with get_database_connection() as conn:
-                cursor = conn.cursor()
-                cursor.execute("SELECT id, password FROM users WHERE username = %s", (username,))
-                user = cursor.fetchone()
+                cmd, username, password = cmd_info.split(' ', 2)
 
+                # Traitement des commandes REGISTER et LOGIN
                 if cmd == "REGISTER":
-                    if user:
+                    cursor.execute("SELECT id FROM users WHERE username = %s", (username,))
+                    if cursor.fetchone():
                         client_socket.send("Username is already taken. Please try a different username.".encode())
-                        continue  # Permet à l'utilisateur de réessayer
+                        continue
                     else:
-                        # Enregistrement de l'utilisateur
                         hashed_password = hash_password(password)
                         cursor.execute("INSERT INTO users (username, password, ip_address, last_login) VALUES (%s, %s, %s, NOW())",
                                        (username, hashed_password, str(client_address[0])))
                         conn.commit()
                         client_socket.send("Registration successful!".encode())
-                        break  # Sortir de la boucle après un enregistrement réussi
+                        break
 
                 elif cmd == "LOGIN":
+                    cursor.execute("SELECT id, password FROM users WHERE username = %s", (username,))
+                    user = cursor.fetchone()
                     if user and verify_password(user[1], password):
-                        # Vérifier si l'utilisateur est banni ou expulsé
-                        if client_address[0] in banned_ips or (username in kicked_users and time.time() < kicked_users[username]):
-                            client_socket.send("You are banned or kicked from the server.".encode())
-                            client_socket.close()
-                            continue  # Permet à l'utilisateur de réessayer
-
                         client_socket.send("Login successful!".encode())
-                        break  # Sortir de la boucle après une connexion réussie
+                        break
                     else:
                         client_socket.send("Invalid login credentials.".encode())
-                        continue  # Permet à l'utilisateur de réessayer
+                        continue
 
-        # Ajout du client à la liste des clients connectés et gestion des messages
-        clients[username] = (client_socket, client_address[0])
-        broadcast(f"{username} has joined the chat.", sender_username=username)
-        broadcast_user_list()
+            # Ajouter le client à la liste des clients connectés
+            clients[username] = (client_socket, client_address[0])
+            broadcast(f"{username} has joined the chat.", sender_username=username)
+            broadcast_user_list()
 
-        # Gestion des messages entrants
-        while not shutdown_flag.is_set():
-            message = client_socket.recv(1024).decode()
+            # Gestion des messages entrants
+            while not shutdown_flag.is_set():
+                message = client_socket.recv(1024).decode()
 
-            # Traitement des commandes pour les salons
-            if message.startswith('/join'):
-                _, room_name = message.split()
-                response = join_room(cursor, username, room_name)
-                client_socket.send(response.encode())
+                if message.startswith('/join'):
+                    _, room_name = message.split()
+                    response = join_room(cursor, username, room_name)
+                    client_socket.send(response.encode())
 
-            elif message.startswith('/leave'):
-                _, room_name = message.split()
-                response = leave_room(cursor, username, room_name)
-                client_socket.send(response.encode())
+                elif message.startswith('/leave'):
+                    _, room_name = message.split()
+                    response = leave_room(cursor, username, room_name)
+                    client_socket.send(response.encode())
 
-            elif message.startswith('/msg'):
-                _, room_name, room_message = message.split(' ', 2)
-                response = send_room_message(cursor, username, room_name, room_message)
-                client_socket.send(response.encode())
+                elif message.startswith('/msg'):
+                    _, room_name, room_message = message.split(' ', 2)
+                    response = send_room_message(cursor, username, room_name, room_message)
+                    client_socket.send(response.encode())
 
-            # Traitement des messages normaux et privés
-            elif message.startswith("@"):
-                # Logique pour les messages privés
-                if send_private_message(username, message):
-                    continue
+                elif message.startswith("@"):
+                    # Logique pour les messages privés
+                    if send_private_message(username, message):
+                        continue
 
-            else:
-                # Logique pour les messages publics
-                broadcast(f"{username} says: {message}", sender_username=username)
+                else:
+                    # Logique pour les messages publics
+                    broadcast(f"{username} says: {message}", sender_username=username)
 
     except ConnectionResetError:
         print(f"Connection reset by peer: {client_address}")
     except socket.error as e:
-        if e.errno == errno.WSAECONNABORTED:
-            print(f"Connection aborted: {client_address}")
-        else:
-            print(f"Error in handle_client: {e}")
+        print(f"Error in handle_client: {e}")
     except Exception as e:
         print(f"Error in handle_client: {e}")
     finally:
@@ -346,6 +338,7 @@ def handle_client(client_socket, client_address):
             if not shutdown_flag.is_set():
                 broadcast(f"{username} has disconnected.")
         client_socket.close()
+
 
 def fictiousclient():
     socketcli = socket.socket()
